f3e43111882a2eee5fdf2234ba716bacc39cf933
Amanda-Alves-SC <amandaalvesdesousa2980@gmail.com>
2025-11-13 20:02:16 -0300

    Database config

---
+++ app/conexao.py
@@ -0,0 +1,11 @@
+import os
+
+config = {
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT"))
+}
+
+key = os.getenv("GOOGLE_MAPS_API_KEY")
